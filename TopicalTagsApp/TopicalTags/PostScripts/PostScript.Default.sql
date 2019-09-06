b8bd91efa1c1efb69cd8a9154dafec91c9847b11
dojo87 <dojo@op.pl>
2019-09-06 11:50:56 +0200

    PostScript.Default.sql

---
+++ TopicalTagsApp/TopicalTags/PostScripts/PostScript.Default.sql
@@ -0,0 +1,7 @@
+﻿/*
+Post-Deployment Script Template
+This file will be taken as default if no environment specific script will be found. 
+You should not edit PostScript.sql as it is generated. If you want to edit default values, edit PostScript.Default.sql
+*/
+
+INSERT INTO Topics (Id, Title, Url) VALUES (100, 'DEFAULT','localhost');
